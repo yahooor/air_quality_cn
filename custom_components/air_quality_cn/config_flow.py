f0dc97cb80e5c4b3352e86ffa25fc199356911f2
yahooor <betamail@163.com>
2026-04-29 22:50:58 +0800

    chore: 发布 v2.4.0

--- custom_components/air_quality_cn/config_flow.py
+++ custom_components/air_quality_cn/config_flow.py
@@ -23,7 +23,7 @@ _LOGGER = logging.getLogger(__name__)
 
 
 class AirQualityCNConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    VERSION = "2.3.9"
+    VERSION = "2.4.0"
 
     def __init__(self):
         self._search_results = []
