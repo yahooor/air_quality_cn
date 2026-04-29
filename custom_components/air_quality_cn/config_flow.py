756405129f655c9264d64ecced1e42c5fa75bae9
yahooor <betamail@163.com>
2026-04-29 23:35:45 +0800

    clean: 删除 locations.json（44MB，代码无引用）

--- custom_components/air_quality_cn/config_flow.py
+++ custom_components/air_quality_cn/config_flow.py
@@ -23,7 +23,7 @@ _LOGGER = logging.getLogger(__name__)
 
 
 class AirQualityCNConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    VERSION = "2.4.2"
+    VERSION = "2.4.3"
 
     def __init__(self):
         self._search_results = []
